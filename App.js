7176bca3cf92ce03aeba21e5451d7c5480a4ee8e
willfbren <willfbren@gmail.com>
2020-08-17 13:54:45 -0500

    Basic structure and card component

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import styled from 'styled-components'; 
+import Card from './components/Card';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      return (
+        <Container>
+            <TitleBar>
+                <Avatar source={require('./assets/avatar.jpg')} />
+                <Title>Welcome Back</Title>
+                <Name>William</Name>
+            </TitleBar>
+            <Subtitle>Continue Learning</Subtitle>
+            <Card />
+        </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Subtitle = styled.Text`
+    color: #b8bece;
+    font-weight: 600;
+    font-size: 15px;
+    margin-left: 20px;
+    margin-top: 50px;
+    text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+    width: 44px;
+    height: 44px;
+    background: black;
+    border-radius: 22px;
+    margin-left: 20px;
+    position: absolute;
+    top: 0;
+    left: 0;
+`;
+
+const Container = styled.View`
+    background: #f0f3f5;
+    flex: 1;
+`;
+
+const TitleBar = styled.View`
+    width: 100%;
+    margin-top: 50px;
+    padding-left: 80px;
+`;
+
+const Title = styled.Text`
+    font-size: 16px;
+    color: #b8bece;
+    font-weight: 500;
+`;
+
+const Name = styled.Text`
+    font-size: 20px;
+    color: #3c4560;
+    font-weight: bold;
+`;
+
+
+
+
