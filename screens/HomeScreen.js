73f21f529e0de93c0ba43321aae7d8ad24a2ad71
willfbren <willfbren@gmail.com>
2020-08-24 15:27:53 -0500

    Added graphql/apollo and query for cards from contentful

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,40 @@ import Course from "../components/Course";
 import Menu from "../components/Menu";
 import { connect } from "react-redux";
 import Avatar from "../components/Avatar";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const CardsQuery = gql`
+    {
+        cardsCollection {
+            items {
+                title
+                subtitle
+                image {
+                    title
+                    description
+                    contentType
+                    fileName
+                    size
+                    url
+                    width
+                    height
+                }
+                caption
+                logo {
+                    title
+                    description
+                    contentType
+                    fileName
+                    size
+                    url
+                    width
+                    height
+                }
+            }
+        }
+    }
+`;
 
 function mapStateToProps(state) {
     return { action: state.action, name: state.name };
@@ -119,11 +153,7 @@ class HomeScreen extends React.Component {
                                 showsHorizontalScrollIndicator={false}
                             >
                                 {logos.map((logo, index) => (
-                                    <Logo
-                                        key={index}
-                                        image={logo.image}
-                                        text={logo.text}
-                                    />
+                                    <Logo key={index} image={logo.image} text={logo.text} />
                                 ))}
                             </ScrollView>
                             <Subtitle>Continue Learning</Subtitle>
@@ -132,24 +162,36 @@ class HomeScreen extends React.Component {
                                 style={{ paddingBottom: 30 }}
                                 showsHorizontalScrollIndicator={false}
                             >
-                                {cards.map((card, index) => (
-                                    <TouchableOpacity
-                                        key={index}
-                                        onPress={() => {
-                                            this.props.navigation.push("Section", {
-                                                section: card
-                                            });
-                                        }}
-                                    >
-                                        <Card
-                                            title={card.title}
-                                            image={card.image}
-                                            caption={card.caption}
-                                            logo={card.logo}
-                                            subtitle={card.subtitle}
-                                        />
-                                    </TouchableOpacity>
-                                ))}
+                                <Query query={CardsQuery}>
+                                    {({ loading, error, data }) => {
+                                        if (loading) return <Message>Loading...</Message>;
+                                        if (error) return <Message>Error...</Message>;
+
+                                        console.log(data.cardsCollection.items);
+                                        return (
+                                            <CardsContainer>
+                                                {data.cardsCollection.items.map((card, index) => (
+                                                    <TouchableOpacity
+                                                        key={index}
+                                                        onPress={() => {
+                                                            this.props.navigation.push("Section", {
+                                                                section: card
+                                                            });
+                                                        }}
+                                                    >
+                                                        <Card
+                                                            title={card.title}
+                                                            image={card.image}
+                                                            caption={card.caption}
+                                                            logo={card.logo}
+                                                            subtitle={card.subtitle}
+                                                        />
+                                                    </TouchableOpacity>
+                                                ))}
+                                            </CardsContainer>
+                                        );
+                                    }}
+                                </Query>
                             </ScrollView>
                             <Subtitle>Popular Courses</Subtitle>
                             {courses.map((course, index) => (
@@ -173,6 +215,17 @@ class HomeScreen extends React.Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+
+const Message = styled.Text`
+    margin: 20px;
+    color: #b8bece;
+    font-size: 15px;
+    font-weight: 500;
+`;
+
+const CardsContainer = styled.View`
+    flex-direction: row;
+`;
 
 const RootView = styled.View`
     background: black;
@@ -308,7 +361,6 @@ const courses = [
         logo: require("../assets/logo-figma.png"),
         author: "Meng To",
         avatar: require("../assets/avatar.jpg"),
-        caption:
-            "Complete guide to designing a site using a collaborative design tool"
+        caption: "Complete guide to designing a site using a collaborative design tool"
     }
 ];
