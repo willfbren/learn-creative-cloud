0a6d490905cadfbbd28adf330993ac5f7f1b9dbf
willfbren <willfbren@gmail.com>
2020-09-05 12:53:33 -0500

    loading and success animation on login

--- components/ModalLogin.js
+++ components/ModalLogin.js
@@ -2,17 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import { TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { BlurView } from "expo-blur";
+import Success from "../components/Success";
+import Loading from "./Loading";
 
 class ModalLogin extends React.Component {
     state = {
         email: "",
         password: "",
         iconEmail: require("../assets/icon-email.png"),
-        iconPassword: require("../assets/icon-password.png")
+        iconPassword: require("../assets/icon-password.png"),
+        isSuccessful: false,
+        isLoading: false
     };
 
     handleLogin = () => {
         console.log(this.state.email, this.state.password);
+
+        this.setState({ isLoading: true });
+
+        setTimeout(() => {
+            this.setState({ isLoading: false });
+            this.setState({ isSuccessful: true });
+        }, 2000);
     };
 
     focusEmail = () => {
@@ -72,6 +83,8 @@ class ModalLogin extends React.Component {
                         </Button>
                     </TouchableOpacity>
                 </Modal>
+                <Success isActive={this.state.isSuccessful} />
+                <Loading isActive={this.state.isLoading} />
             </Container>
         );
     }
